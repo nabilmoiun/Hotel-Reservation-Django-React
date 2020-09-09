d5c02c943f11e082625b9ac37a5728f352a88a2e
Moinul Hossain Nabil <nnaabiill@gmail.com>
2020-09-09 23:38:43 +0600

    updated the sirializer

--- hotel_app/serializer.py
+++ hotel_app/serializer.py
@@ -3,6 +3,10 @@ from rest_framework import serializers
 
 
 class RoomSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.category_name')
     class Meta:
         model = Room
         fields = '__all__'
+        
+    def create(self, validated_data):
+        return super().create(self.category_name)
